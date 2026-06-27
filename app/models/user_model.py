9a311c19b416bb5be689d887b5019b9d04f44255
juki <jukijukirthan12@gmail.com>
2026-06-27 10:40:21 +0530

    model create and user table create

---
+++ app/models/user_model.py
@@ -0,0 +1,12 @@
+from extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from utils import utc_now
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(120), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=utc_now)
